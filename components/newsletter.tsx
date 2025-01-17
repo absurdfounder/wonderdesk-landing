a0c81a765afbeecca3bb61f5577488a864f90327
Vaibhav <qvmail83@gmail.com>
2025-01-17 02:14:29 +0000

    trynow

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -46,7 +46,7 @@ export default function Newsletter() {
               {/* CTA content */}
               <div className="text-center lg:text-left lg:max-w-xl">
                 <h3 className="h3 text-white mb-2">Ready to turn your Notion pages into <span className=" -translate-y-full slideUp h-full w-full bg-clip-text text-transparent text-orange-600">{words[index]}</span> ?</h3>
-                <p className="text-xl text-slate-600 mb-4">Join the BoringSites AI and discover the easiest way to manage your website.</p>
+                <p className="text-xl text-slate-400 mb-4">Join the BoringSites AI and discover the easiest way to manage your website.</p>
 
                 {/* CTA form */}
                 <form className="w-full lg:w-auto">
