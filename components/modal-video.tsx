c34103351a3c65b641a158844bcde12ddec8b00e
Vaibhav <qvmail83@gmail.com>
2025-01-31 21:25:21 +0000

    trynow

--- components/modal-video.tsx
+++ components/modal-video.tsx
@@ -56,14 +56,12 @@ export default function ModalVideo({
           </div>
 
           <button 
-            className="relative top-full flex items-center transform -translate-y-1/2 rounded-full group p-4 shadow-lg border border-4 border-slate-100 bg-orange-400 text-slate-800 m-auto text-xl font-bold px-6" 
+            className="absolute -mt-64 flex items-center transform -translate-y-1/2 m-auto justify-center w-full" 
             onClick={() => { setModalOpen(true) }}
           >
-            <svg className="w-6 h-6 fill-current text-slate-900 group-hover:text-blue-600 shrink-0" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-10 12z" />
-              <path d="M10 17l6-5-6-5z" />
-            </svg>
-            <span className="ml-3">Watch me build</span>
+
+<img src="https://dazzling-cat.netlify.app/sticky.webp"/>
+
           </button>
         </div>
 
