6e17066e31b0c07d7bd53a17dea7594cf51f91f3
Vaibhav <qvmail83@gmail.com>
2025-03-15 22:46:47 +0000

    trynow

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -34,7 +34,6 @@ import Newsletter from '@/components/newsletter'
 import Marketplace_Type from '@/components/marketplaces_type'
 import FAQ from '@/components/faq'
 import ModalVideo from '@/components/modal-video'
-import SchemaMarkup from '@/components/SchemaMarkup'
 
 export default function Home() {
   return (
@@ -54,7 +53,6 @@ export default function Home() {
       <FAQ />
       <AboutMe />
       <Newsletter />
-      <SchemaMarkup />
     </>
   )
 }
