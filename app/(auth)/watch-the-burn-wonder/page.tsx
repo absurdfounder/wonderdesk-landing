f183f9f019ffd928585b32157af4e1f7dc85b23f
mav <78248941+mavbuilds@users.noreply.github.com>
2025-05-26 15:36:36 +0000

    trynow

---
+++ app/(auth)/watch-the-burn-wonder/page.tsx
@@ -0,0 +1,135 @@
+
+import React, { useState, useEffect } from "react";
+
+import ModalVideo from "@/components/modal-video";
+import VideoThumb from "@/public/images/hero-image.png";
+
+import Link from "next/link";
+import Image from "next/image";
+
+import TrustedBy from "../compare-against/TrustedBy";
+import FeaturesBlocks from "@/components/features-blocks";
+import Blog_Type from "@/components/blog_type";
+
+import Testimonials from "@/components/testimonials";
+import Header from "@/components/ui/header";
+import BelieveLandingPage from "@/components/token-page";
+import WondersitesDashboard from "@/components/watchburn";
+
+
+import Aos from "aos";
+import 'aos/dist/aos.css'
+
+
+export const metadata = {
+    title: "Get a blog with Notion (no-code and free)",
+    description:
+        "Build a blog in Notion, with our Notion blog maker. Go live in under a minute.  Build your free Wonder  blog website in a minute!",
+        alternates: {
+            canonical: "https://wondersites.co/create-a-blog-notion",
+          },
+    openGraph: {
+        images: [
+            {
+                url: "https://dazzling-cat.netlify.app/notiontoblog_socialshare.png",
+                width: 1200,
+                height: 630,
+                alt: "Get a blog with Notion",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        images: [
+            {
+                url: "https://dazzling-cat.netlify.app/notiontoblog_socialshare.png",
+                alt: "Get a blog with Notion",
+            },
+        ],
+    },
+};
+
+export default function PageDetail() {
+    return (
+
+
+        
+        <div>
+
+      <div
+        style={{
+          backgroundImage: "linear-gradient(rgb(255 255 255 / 0%), rgba(255, 255, 255, 0)), url(https://dazzling-cat.netlify.app/cloudbackground.png)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundAttachment: "fixed"
+        }}
+      >
+
+
+            <section >
+
+                <Header />
+
+
+
+                <div className="max-w-7xl mx-auto px-4 sm:px-6">
+                    <div className="pt-12 pb-6 md:pt-4 md:pb-6">
+                        {/* Page header */}
+
+                        <div className="max-w-4xl py-6 mx-auto lg:py-6">
+
+
+                            <div className="text-center">
+
+
+                                <div className="max-w-screen-xl px-4 mx-auto sm:px-6 mb-4">
+                                    <div className="text-center px-4 sm:px-6 lg:px-8">
+                                        <h1 className="font-funneldisplay text-3xl sm:text-4xl md:text-7xl font-bold tracking-tight text-slate-900 leading-tighter tracking-tighter mb-4 aos-init aos-animate">
+                                            watch the <span className="text-3xl sm:text-4xl md:text-7xl gradient-text-accent mx-4">burn</span>
+                                        </h1>
+                                        <p className="max-w-md mx-auto mt-3 text-base text-slate-500 sm:mb-0 sm:text-lg md:mt-5 md:text-lg md:max-w-2xl">
+                                            Wonder token powers are <b>burned with every action</b> signup, website creation, or every when they get traffic on their website, scarecity rises as wonder grows.
+                                        </p>
+                                        <div className="flex flex-col-reverse justify-center w-full mt-10 sm:flex-row">
+                                            <div className="mt-3 sm:mt-0">
+                                                <Link href="/signup" className="inline-flex items-center justify-center w-full px-6 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out rounded-md shadow bg-orange-700 sm:w-auto">
+                                                    <strong className="mr-1">Verify on SolScan</strong>
+                                                </Link>
+                                                <p className="mt-3 text-sm text-slate-700"><strong>16720</strong> $wonder burned 60 mins ago.</p>
+                                            </div>
+                                        </div>
+
+
+
+                                    </div>
+
+                                </div>
+
+
+                            </div>
+                        </div>
+
+<WondersitesDashboard/>
+
+
+                        <ModalVideo
+                        />
+
+                    </div>
+                </div>
+
+
+
+            </section>
+
+      </div>
+
+
+            <FeaturesBlocks />
+
+            <Testimonials />
+
+        </div>
+
+    );
+}
