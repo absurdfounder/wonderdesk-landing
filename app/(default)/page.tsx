ecc5c8aff39b6a0fc715185ea6744e3d06d65617
Vaibhav <qvmail83@gmail.com>
2025-09-29 15:04:35 +0000

    trynow

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -54,51 +54,51 @@ export default function Home() {
       >
         <Header />
         <Hero />
-        
+
         {/** 
         <WonderComparison/>
         */}
       </div>
 
-    <section className="px-4 py-6 md:px-6 md:py-10 border-b">
-      <div className="mx-auto max-w-5xl">
-        <div className="flex flex-col items-center justify-between gap-4 md:flex-row md:items-center">
-          <span className="mt-1 text-base font-medium text-balance text-gray-400 md:text-left">
-            Trusted by leading product-led companies
-          </span>
-          <div className="mx-auto flex flex-wrap place-items-center items-center justify-center gap-8">
-            <div className="h-12 max-h-12 w-32 transition-all duration-300 hover:scale-110">
-              <img
-                className="h-full w-full origin-center object-contain opacity-50 grayscale transition-all duration-300 hover:opacity-100 hover:grayscale-0"
-                src="https://dazzling-cat.netlify.app/logos/zeroslistlogo.png"
-                alt="Transistor"
-              />
-            </div>
-            <div className="h-12 max-h-12 w-32 transition-all duration-300 hover:scale-110">
-              <img
-                className="h-full w-full origin-center object-contain opacity-50 grayscale transition-all duration-300 hover:opacity-100 hover:grayscale-0"
-                src="https://dazzling-cat.netlify.app/logos/marketingxlogo.png"
-                alt="Gummy Search"
-              />
-            </div>
-            <div className="h-12 max-h-12 w-32 transition-all duration-300 hover:scale-110">
-              <img
-                className="h-full w-full origin-center object-contain opacity-50 grayscale transition-all duration-300 hover:opacity-100 hover:grayscale-0"
-                src="https://dazzling-cat.netlify.app/logos/dealflowlogo.png"
-                alt="Right Message"
-              />
-            </div>
-            <div className="h-12 max-h-12 w-32 transition-all duration-300 hover:scale-110">
-              <img
-                className="h-full w-full origin-center object-contain opacity-50 grayscale transition-all duration-300 hover:opacity-100 hover:grayscale-0"
-                src="https://dazzling-cat.netlify.app/logos/downtownlogo.png"
-                alt="Company 4"
-              />
+      <section className="px-4 py-6 md:px-6 md:py-10 border-b">
+        <div className="mx-auto max-w-5xl">
+          <div className="flex flex-col items-center justify-between gap-4 md:flex-row md:items-center">
+            <span className="mt-1 text-base font-medium text-balance text-gray-400 md:text-left">
+              Trusted by leading product-led companies
+            </span>
+            <div className="mx-auto flex flex-wrap place-items-center items-center justify-center gap-8">
+              <div className="h-12 max-h-12 w-32 transition-all duration-300 hover:scale-110">
+                <img
+                  className="h-full w-full origin-center object-contain opacity-50 grayscale transition-all duration-300 hover:opacity-100 hover:grayscale-0"
+                  src="https://dazzling-cat.netlify.app/logos/zeroslistlogo.png"
+                  alt="Transistor"
+                />
+              </div>
+              <div className="h-12 max-h-12 w-32 transition-all duration-300 hover:scale-110">
+                <img
+                  className="h-full w-full origin-center object-contain opacity-50 grayscale transition-all duration-300 hover:opacity-100 hover:grayscale-0"
+                  src="https://dazzling-cat.netlify.app/logos/marketingxlogo.png"
+                  alt="Gummy Search"
+                />
+              </div>
+              <div className="h-12 max-h-12 w-32 transition-all duration-300 hover:scale-110">
+                <img
+                  className="h-full w-full origin-center object-contain opacity-50 grayscale transition-all duration-300 hover:opacity-100 hover:grayscale-0"
+                  src="https://dazzling-cat.netlify.app/logos/dealflowlogo.png"
+                  alt="Right Message"
+                />
+              </div>
+              <div className="h-12 max-h-12 w-32 transition-all duration-300 hover:scale-110">
+                <img
+                  className="h-full w-full origin-center object-contain opacity-50 grayscale transition-all duration-300 hover:opacity-100 hover:grayscale-0"
+                  src="https://dazzling-cat.netlify.app/logos/downtownlogo.png"
+                  alt="Company 4"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
 
       <NotiontoWebsite />
       {/* 
