c3b432d0e7223833191360f3c4053064861a58ee
StrawhatLuffy <qvmail83@gmail.com>
2025-10-29 23:56:05 +0530

    Trynow

--- components/features-blocks.tsx
+++ components/features-blocks.tsx
@@ -13,7 +13,7 @@ export default function FeaturesBlocks() {
             {/* Section header */}
             <div className="max-w-3xl mx-auto text-center pt-12 mb-8 ">
               <h2 className="font-funneldisplay h2 mb-4">
-                Built for both people and AI
+                Get started in minutes
               </h2>
 
               <p className="text-xl text-slate-600">We know you have too much on your plate, this is why Wonder exists to help you setup <span className=" font-normal text-orange-600 border-b">marketing and support on automation</span>.</p>
